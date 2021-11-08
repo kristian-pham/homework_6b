96d137ee13b59a367e502796d5ab56749782c9f5
kristian-pham <91481232+kristian-pham@users.noreply.github.com>
2021-11-07 19:37:19 -0500

    fixed bugs

--- main.js
+++ main.js
@@ -17,35 +17,46 @@ function itemAdded() {
 
 // this is the start of the cart functionality
 
-/* let carts = document.querySelectorAll('.add-cart');
-
-for (let i = 0; i <= carts.length; i++) {
-  console.log(carts.length);
-  carts[i].addEventListener('click', function () {
-    console.log('here');
-  })
-} */
-
-//every time you click the button, number goes up
-
 //set cart quantity to 0 by default
 let cartQty = 0;
-console.log('here ' + cartQty);
 
-//every time you click, add one to cart
-function cartAddOne() {
+//used every time product-detail "add to cart" is clicked
+function cartCounter() {
   cartQty += 1;
   localStorage.setItem('cartNumber', cartQty);
+  updateCartNav()
+}
+
+//every time you click, add one to cart
+//should be updating the navbar cart qty, on load of each screen
+function updateCartNav() {
   document.getElementById('clicks').innerHTML = " (" + localStorage.getItem('cartNumber') + ")";
+}
+
+// need to transfer saved customizations from server to Cart
+
+// need to delete items in cart
+function removeItem() {
+  cartQty - 1;
+  localStorage.setItem('cartNumber', cartQty);
+  updateCartNav();
 }
 
 // cart page display
 // when var cartQty is zero, show "your cart is empty"
 
+function emptyCart (){
+  if (parseInt(localStorage.getItem('cartNumber')) > 1) {
+    console.log(parseInt(localStorage.getItem('cartNumber')));
+    console.log('if');
+    document.getElementById('noItems').style.display = "none";
+    document.getElementById('noItems').innerHTML = "";
+  } else {
+    console.log(parseInt(localStorage.getItem('cartNumber')));
+    console.log('else');
+    //document.getElementById("itemInCart").style.display = "none";
+    document.getElementById('itemInCart').innerHTML = "hi";
+  }
+}
 
-// need to save customizations from product-details page to server
-//after customizations are selected, save the
-
-// need to transfer saved customizations from server to Cart
-
-// need to delete items in cart
+emptyCart()
